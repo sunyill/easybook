b16754875309238622987985b3ef41fd3bb7c0e9
wang.zhan <sunyill0707@163.com>
2017-12-25 19:42:09 +0800

    10,使用immutable.js库对初始化数据管理test1

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,18 +1,16 @@
 import * as constant from './constant'
-const defaultDate = {
+import  { fromJS } from 'immutable' 
+const defaultDate = fromJS({
     focused:false
-}
+})
 
 export default (state = defaultDate, action )=>{
     if(action.type === constant.TYPE_FOCUS){
-        return {
-            focused:true
-        }
+        return state.set('focused',true)
+        
     }
     if(action.type === constant.TYPE_BLUR){
-        return {
-            focused:false
-        }
+        return state.set('focused',false)
     }
     return state
 }
