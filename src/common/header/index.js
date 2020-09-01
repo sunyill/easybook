aa64d3197bcd42cbe7d1e858672b75d35e7d4816
wang.zhan <sunyill0707@163.com>
2020-09-01 17:51:27 +0800

    4 easybook头布局基本完成

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
-import {Headers, Logo} from './style'
+import {Headers, Logo,Nav,NavItem,Search,AddContent,Button} from './style'
 
 class Header extends Component {
     render (){
         return (
             <Headers>
-                <Logo href="/"/>
+                <Logo/>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">Aa</NavItem>
+                    <NavItem className="right">登录</NavItem>
+                    <Search></Search>
+                </Nav>
+                <AddContent>
+                    <Button className='writting'>写文章</Button>
+                    <Button className= 'regis'>注册</Button>
+                </AddContent>
             </Headers>
+            
         )
     }
 }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -19,7 +19,73 @@ export const Logo = styled.a.attrs({
     top:0;
     left:0;
     background:url(${logoImage});
-    background-size:contain;
+    background-size:contain;  
+`
 
+export const Nav =styled.div`
+    width:960px;
+    height:56px;
+    padding-right:70px;
+    line-height:56px;
+    margin: 0 auto;
+`
+
+export const NavItem = styled.div`
+    font-size:17px;
+    color:#333;
+    margin-right:30px;
+    &.active {
+        color:#ea6f5a;
+    }
+    &.left{
+        float:left;
+    }
+    &.right {
+        float:right;
+        color:#969696;
+    }
+`
+
+export const Search = styled.input.attrs({
+    placeholder:"搜索"
+})`
+    width:160px;
+    height:38px;
+    padding: 0 20px;
+    border:none;
+    outline:none;
     
+    box-sizing:border-box;
+    border-radius:19px;
+    font-size:14px;
+    background:#eee;
+    &::placeholder{
+        color:#999;
+    }
+`
+
+export const AddContent = styled.div`
+    position:absolute;
+    top:0;
+    right:0;
+    height:56px;
+    
+`
+
+export const Button = styled.button`
+    float:right;
+    margin-top:9px;
+    margin-right:20px;
+    padding: 0 20px;
+    height:38px;
+    border-radius:19px;
+    border:1px solid #ea6f5a;
+    &.writting{
+        color:#fff;
+        background:#ea6f5a;
+    }
+    &.regis {
+        clolr:#ea6f5a;
+        background:#fff;
+    }
 `
