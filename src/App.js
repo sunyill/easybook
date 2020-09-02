9a342d06a386a1ce3dea60b5ee56a1c9a85c38ba
wang.zhan <sunyill0707@163.com>
2020-09-02 16:24:28 +0800

    7. 使用react-redux 进行数据管理

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React , {Component, Fragment} from 'react';
 import Header from './common/header'
+import { Provider } from 'react-redux'
+import store from './store'
+
 import { GlobalStyled } from './style' 
 import { IconFontStyled } from './statics/iconfont/iconfont'
 class App extends Component {
   render(){
     return (
       <Fragment>
-        <GlobalStyled/>
-        <IconFontStyled/>
-        <Header></Header>
+        <Provider store = { store }>
+          <GlobalStyled/>
+          <IconFontStyled/>
+          <Header></Header>
+        </Provider>
       </Fragment>
     )
   }
