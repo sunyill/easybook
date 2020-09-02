dc16199b361fe77efbe248682cfc4e02b9f051c0
wang.zhan <sunyill0707@163.com>
2020-09-02 16:29:27 +0800

    8.修改header为无状态组件,提高性能

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Headers, Logo,Nav,NavItem,SearchWrapper,Search,AddContent,Button} from './style'
 import { CSSTransition } from 'react-transition-group'
-class Header extends Component {
-    render (){
-        return (
-            <Headers>
-                <Logo/>
-                <Nav>
-                    <NavItem className="left active">首页</NavItem>
-                    <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">
-                        <i className="iconfont">&#xe636;</i>
-                    </NavItem>
-                    <NavItem className="right">登录</NavItem>
-                    <SearchWrapper>
-                        <CSSTransition
-                            timeout ={ 2000 }
-                            in= {this.props.focused}
-                            classNames = 'slide'
-                        >
-                            <Search 
-                            className= {this.props.focused? "focused":''}
-                            onFocus = {this.props.onFocusHandle}
-                            onBlur = {this.props.onBlurHandle}
-                            ></Search>
-                        </CSSTransition>
-                            <i className= {this.props.focused? "focused iconfont":'iconfont'}>&#xe681;</i>
-                    </SearchWrapper>
-                </Nav>
-                <AddContent>
-                    <Button className='writting'>
-                        <i className="iconfont">&#xe676;</i>
-                        写文章</Button>
-                    <Button className= 'regis'>注册</Button>
-                </AddContent>
-            </Headers>
-            
-        )
-    }
+
+const Header = (props)=>{
+            return (
+                <Headers>
+                    <Logo/>
+                    <Nav>
+                        <NavItem className="left active">首页</NavItem>
+                        <NavItem className="left">下载App</NavItem>
+                        <NavItem className="right">
+                            <i className="iconfont">&#xe636;</i>
+                        </NavItem>
+                        <NavItem className="right">登录</NavItem>
+                        <SearchWrapper>
+                            <CSSTransition
+                                timeout ={ 2000 }
+                                in= {props.focused}
+                                classNames = 'slide'
+                            >
+                                <Search 
+                                className= {props.focused? "focused":''}
+                                onFocus = {props.onFocusHandle}
+                                onBlur = {props.onBlurHandle}
+                                ></Search>
+                            </CSSTransition>
+                                <i className= {props.focused? "focused iconfont":'iconfont'}>&#xe681;</i>
+                        </SearchWrapper>
+                    </Nav>
+                    <AddContent>
+                        <Button className='writting'>
+                            <i className="iconfont">&#xe676;</i>
+                            写文章</Button>
+                        <Button className= 'regis'>注册</Button>
+                    </AddContent>
+                </Headers>
+                
+            )
 }
 
 const mapStateToProps = (state)=>{
@@ -50,7 +49,6 @@ const mapStateToProps = (state)=>{
 const mapDispathToProps = (dispath)=>{
     return {
         onFocusHandle(){
-            console.log("123");
             const action = {
                 type:'type_focus'
             }
